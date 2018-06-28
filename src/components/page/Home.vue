ae05f64c6921908bbf9e3e79c6b01881a7d35249
zlw <460870550@qq.com>
2018-06-28 13:34:41 +0800

    首页新增todoList模块

--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -60,6 +60,38 @@
             </el-card>
           </el-col>
         </el-row>
+        <el-row>
+          <el-card shadow="hover" :body-style="{ height: '304px'}">
+            <div slot="header" class="clearfix">
+              <span>待办事项</span>
+              <el-button @click="handleToDoListAdd" style="float: right; padding: 3px 0" type="text">添加</el-button>
+            </div>
+            <el-table ref="multipleTable" :data="todoList" :show-header="false" style="width: 100%">
+              <el-table-column width="40">
+                <template slot-scope="scope">
+                  <el-checkbox v-model="scope.row.status"></el-checkbox>
+                </template>
+              </el-table-column>
+              <el-table-column>
+                <template slot-scope="scope">
+                  <div v-if="!scope.row.editStatus" :class="{'todo-item-del': scope.row.status}">
+                    {{scope.row.title}}
+                  </div>
+                  <span v-if="scope.row.editStatus">
+                    <input class="cell-edit-input" type="text" v-model="scope.row.title" autofocus="autofocus"/>
+                  </span>
+                </template>
+              </el-table-column>
+              <el-table-column width="80">
+                <template slot-scope="scope">
+                  <i v-if="!scope.row.editStatus" @click="handleToDoListEdit(scope.row)" class="todo-item-action el-icon-edit"></i>
+                  <i v-if="scope.row.editStatus" @click="handleToDoListSave(scope.row)" class="todo-item-action el-icon-circle-check-outline"></i>
+                  <i @click="handleToDoListDelete(scope.$index, todoList)" class="todo-item-action el-icon-delete"></i>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-card>
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -76,29 +108,64 @@ export default {
       loginArea: JSON.parse(localStorage.getItem('loginInfo')).loginArea,
       rule: '系统管理员',
       canvasId: 'chart',
-        type: 'ring',
-        width: '100%',
-        height: 'auto',
-        chartData: [
-          {name: '3月', value: 1342},
-          {name: '4月', value: 2123},
-          {name: '5月', value: 1654},
-          {name: '6月', value: 1795},
-        ],
-        options: {
-          padding: 30,
-          bgColor: '#FFFFFF',
-          legendColor: '#000000',
-          legendTop: 50,
-          colorList: ['#1E9FFF', '#13CE66', '#F7BA2A', '#FF4949', '#72f6ff'],
-          radius: 70,
-          innerRadius: 40
-            
+      type: 'ring',
+      width: '100%',
+      height: 'auto',
+      chartData: [
+        {name: '3月', value: 1342},
+        {name: '4月', value: 2123},
+        {name: '5月', value: 1654},
+        {name: '6月', value: 1795},
+      ],
+      options: {
+        padding: 30,
+        bgColor: '#FFFFFF',
+        legendColor: '#000000',
+        legendTop: 50,
+        colorList: ['#1E9FFF', '#13CE66', '#F7BA2A', '#FF4949', '#72f6ff'],
+        radius: 70,
+        innerRadius: 40
+      },
+      todoList: [
+        {
+          title: '发布《杭州市城乡医疗保险新知》公告',
+          editStatus: false,
+          status: false,
+        },
+        {
+          title: '处理新入驻医药商家咨询',
+          editStatus: false,
+          status: false,
+        },
+        {
+          title: '录入新增中草药数据',
+          editStatus: false,
+          status: false,
+        },
+        {
+          title: '多写几个bug',
+          editStatus: false,
+          status: true,
         }
+      ],
     }
   },
   components:{
     Schart
+  },
+  methods: {
+    handleToDoListAdd() {
+
+    },
+    handleToDoListEdit(row) {
+      row.editStatus = true;
+    },
+    handleToDoListSave(row) {
+      row.editStatus = false;
+    },
+    handleToDoListDelete(index, rows) {
+      rows.splice(index, 1);
+    },
   }
 }
 </script>
@@ -168,5 +235,32 @@ export default {
 .info-box .info-box-content .description {
   font-size: 12px;
   color: #777777;
+}
+.todo-item-del {
+  text-decoration: line-through;
+  color: #999999;
+}
+.todo-item-action {
+  cursor: pointer;
+  font-size: 14px;
+  margin-right: 15px;
+}
+.todo-item-action:hover {
+  color: #409EFF;
+}
+.cell-edit-input {
+  width: 70%;
+  height: 17px;
+  line-height: 17px;
+  display: inline-block;
+  padding: 2px 10px;
+  border: 1px solid #f0f0f0;
+  border-radius: 5px;
+  outline: none;
+  transition: border-color 0.2s;
+}
+.cell-edit-input:focus {
+  outline: none;
+  border-color: #409eff;
 }
 </style>
